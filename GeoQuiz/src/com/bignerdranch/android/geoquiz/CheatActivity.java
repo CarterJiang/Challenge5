6a93174b5695214ae1fd865f9ae59702abf35525
CarterJiang <jiangq@susqu.edu>
2015-02-06 01:24:59 -0500

    Initial commit

---
+++ GeoQuiz/src/com/bignerdranch/android/geoquiz/CheatActivity.java
@@ -0,0 +1,85 @@
+package com.bignerdranch.android.geoquiz;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class CheatActivity extends Activity {
+
+    private static final String TAG = "CheatActivity";
+    public static final String EXTRA_ANSWER_IS_TRUE = "com.bignerdranch.android.geoquiz.answer_is_true";
+    public static final String EXTRA_ANSWER_SHOWN = "com.bignerdranch.android.geoquiz.ANSWER_SHOWN";
+    private static final String KEY_INDEX = "index";
+
+    boolean mCurrentCheatState=false;
+
+
+
+    boolean mAnswerIsTrue;
+
+    TextView mAnswerTextView;
+    Button mShowAnswer;
+
+
+    private void setAnswerShownResult(boolean isAnswerShown) {
+        Intent data = new Intent();
+        data.putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown);
+        setResult(RESULT_OK, data);
+    }
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate() called");
+        setContentView(R.layout.activity_cheat);
+
+
+        if (savedInstanceState == null) {
+            // first startup, so the answer must not
+            // be shown yet
+            setAnswerShownResult(mCurrentCheatState);
+
+        }
+
+        mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
+
+        mAnswerTextView = (TextView)findViewById(R.id.answerTextView);
+
+        mShowAnswer = (Button)findViewById(R.id.showAnswerButton);
+        mShowAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mAnswerIsTrue) {
+                    mAnswerTextView.setText(R.string.true_button);
+                } else {
+                    mAnswerTextView.setText(R.string.false_button);
+                }
+                mCurrentCheatState=true;
+                setAnswerShownResult(mCurrentCheatState);
+
+            }
+        });
+        if (savedInstanceState != null) {
+            mCurrentCheatState = savedInstanceState.getBoolean(KEY_INDEX);
+            if (mAnswerIsTrue) {
+                mAnswerTextView.setText(R.string.true_button);
+            } else {
+                mAnswerTextView.setText(R.string.false_button);
+            }
+
+        }
+        setAnswerShownResult(mCurrentCheatState);
+    }
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putBoolean(KEY_INDEX, mCurrentCheatState);
+        savedInstanceState.putBoolean(EXTRA_ANSWER_IS_TRUE, mAnswerIsTrue);
+
+
+}}
